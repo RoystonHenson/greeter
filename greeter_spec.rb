f421becb0166529424d28b2ca735d9cd4c3ccf24
Royston <rlqhenson@hotmail.com>
2016-09-22 18:39:50 +0100

    added failing test case to 'greeter_spec.rb'

--- greeter_spec.rb
+++ greeter_spec.rb
@@ -4,4 +4,7 @@ describe "Greeter" do
   it "greets Rico" do
     expect(greet("Rico")).to eq "Hello, Rico, how are you today?"
   end
+  it "greets Chloë" do
+    expect(greet("Chloë")).to eq "Hello, Chloë, how are you today"
+  end
 end
